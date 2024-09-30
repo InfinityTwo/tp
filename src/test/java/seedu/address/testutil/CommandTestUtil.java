a0a726f4841cc0d26cde68e17a1514ed19f19c6d
InfinityTwo <2poh.junkang@gmail.com>
2024-09-30 17:18:20 +0800

    Add remark command

---
+++ src/test/java/seedu/address/testutil/CommandTestUtil.java
@@ -0,0 +1,9 @@
+package seedu.address.testutil;
+
+/**
+ * A utility class to help command remarks.
+ */
+public class CommandTestUtil {
+    public static final String VALID_REMARK_AMY = "Like skiing.";
+    public static final String VALID_REMARK_BOB = "Favourite pastime: Eating";
+}
